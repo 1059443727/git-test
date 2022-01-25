221f6a31a6a2e08974fdc2cd0d4743623b66df77
zxin <1059443727@qq.com>
2022-01-25 14:16:20 +0800

    test5

--- src/main/java/com/atguigu/git/GitTest2.java
+++ src/main/java/com/atguigu/git/GitTest2.java
@@ -11,5 +11,6 @@ public class GitTest2 {
         System.out.println("hello world!");
         System.out.println("hello world!");
         System.out.println("hot-fix");
+        System.out.println("push");
     }
 }
